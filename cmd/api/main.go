de06b67879ee34cf5097eaae37a44c3170583986
Robi9 <robertasousa714@gmail.com>
2026-04-05 16:20:34 -0300

    Remove unused imports and commented-out password hashing code from main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Robi9/church-manager/internal/config"
 	"github.com/Robi9/church-manager/internal/database"
 	"github.com/Robi9/church-manager/internal/server"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-
-	hash, _ := bcrypt.GenerateFromPassword([]byte("123456"), bcrypt.DefaultCost)
-	fmt.Println(string(hash))
-
 	cfg := config.Load()
 
 	database.RunMigrations(cfg.DatabaseURL)
